b6907dd251636eca0c2d42e4b3f66337f8e57a6d
sanjyamf <32727687+sanjyamf@users.noreply.github.com>
2017-10-18 19:51:24 +0700

    Add files via upload

---
+++ Cara.java
@@ -0,0 +1,20 @@
+public class Cara{ // Nama Class 
+public static void main(String [] args){ // Main Method 
+System.out.println( "##################### " );
+System.out.println( " cara memasak nasi  tradisional" );
+System.out.println( "######################" );
+System.out.println( " ==================================================================== " );
+System.out.println( " 1.Siapkan dulu berasnya. Jangan lupa cuci dulu ya sampai bersih. " );
+System.out.println( " 2.Saat kita mulai membersihkan berasnya, isi kukusan kamu dengan air.  " ); 
+System.out.println( " 3.Didihkan kukusan kalian yang sudah terisi air.  . " ); 
+System.out.println( "Tutup dan tunggu sampai air mendidih " );
+System.out.println( " 4.asukkan beras  yang sudah dicuci ke dalam kukusan yang sudah panas. " );
+System.out.println( " 5.Tunggu sekitar 15 menitan. Cek apakah berasnya sudah lumayan lembek " );  
+System.out.println( " 6.Angkat nasi seperempat matang kamu. Sekarang kamu ambil panci.. " );  
+System.out.println( " 7.Kalau sudah ada, masukkan nasi keras tadi ke panci. " );   
+System.out.println( " 8.Tunggu beberapa saat hingga nasi telah matang sempurna . " );
+System.out.println( "======================================================================== " ); // Inputan
+
+
+}
+}
